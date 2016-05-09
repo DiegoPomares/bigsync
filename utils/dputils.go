868ce9410b0c00d095e0ac3a546c656767902ebe
Diego Pomares <dieamare@u5065f33c6b8457028449.ant.amazon.com>
2016-05-09 12:45:30 +0100

    .

---
+++ utils/dputils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func Stderrln(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+func Stderrf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+}
+
+
+func Iserror(err error, args ...interface{}) bool {
+	if err != nil {
+		Stderrf("%s. ", err)
+		Stderrln(args...)
+	
+		return true
+	}
+
+	return false
+}
